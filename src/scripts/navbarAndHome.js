3c81edaebe5313ad8bb700c503e855a3e56dc8eb
Anthony Nikolaidis <a.nikolaidis@student.tudelft.nl>
2021-07-12 11:16:16 +0300

    Remove particles to improve performance

--- src/scripts/navbarAndHome.js
+++ src/scripts/navbarAndHome.js
@@ -46,51 +46,51 @@ let typewriter = new Typed('#home-attributes-span', {
 });
 
 // Particles
-particlesJS("home-particles", {
-  "particles": {
-    "number": {"value": 100, "density": {"enable": true, "value_area": 800}},
-    "color": {"value": ["#fafafa", "#ffffff", "#eeeeee"]},
-    "shape": {
-      "type": "circle",
-      "stroke": {"width": 0, "color": "#000000"},
-      "polygon": {"nb_sides": 5},
-      "image": {"src": "img/github.svg", "width": 100, "height": 100}
-    },
-    "opacity": {
-      "value": 0.15,
-      "random": true,
-      "anim": {"enable": false, "speed": 0.1, "opacity_min": 0.1, "sync": true}
-    },
-    "size": {"value": 2, "random": true, "anim": {"enable": false, "speed": 10, "size_min": 0.1, "sync": false}},
-    "line_linked": {"enable": true, "distance": 150, "color": "#eeeeee", "opacity": 0.15, "width": 0.5},
-    "move": {
-      "enable": true,
-      "speed": 1,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {"enable": false, "rotateX": 600, "rotateY": 1200}
-    }
-  },
-  "interactivity": {
-    "detect_on": "window",
-    "events": {
-      "onhover": {"enable": false, "mode": "bubble"},
-      "onclick": {"enable": false, "mode": "push"},
-      "resize": true
-    },
-    "modes": {
-      "grab": {"distance": 400, "line_linked": {"opacity": 1}},
-      "bubble": {"distance": 300, "size": 15, "duration": 2, "opacity": 0.2, "speed": 3},
-      "repulse": {"distance": 200, "duration": 0.4},
-      "push": {"particles_nb": 4},
-      "remove": {"particles_nb": 2}
-    }
-  },
-  "retina_detect": true
-});
+// particlesJS("home-particles", {
+//   "particles": {
+//     "number": {"value": 100, "density": {"enable": true, "value_area": 800}},
+//     "color": {"value": ["#fafafa", "#ffffff", "#eeeeee"]},
+//     "shape": {
+//       "type": "circle",
+//       "stroke": {"width": 0, "color": "#000000"},
+//       "polygon": {"nb_sides": 5},
+//       "image": {"src": "img/github.svg", "width": 100, "height": 100}
+//     },
+//     "opacity": {
+//       "value": 0.15,
+//       "random": true,
+//       "anim": {"enable": false, "speed": 0.1, "opacity_min": 0.1, "sync": true}
+//     },
+//     "size": {"value": 2, "random": true, "anim": {"enable": false, "speed": 10, "size_min": 0.1, "sync": false}},
+//     "line_linked": {"enable": true, "distance": 150, "color": "#eeeeee", "opacity": 0.15, "width": 0.5},
+//     "move": {
+//       "enable": true,
+//       "speed": 1,
+//       "direction": "none",
+//       "random": false,
+//       "straight": false,
+//       "out_mode": "out",
+//       "bounce": false,
+//       "attract": {"enable": false, "rotateX": 600, "rotateY": 1200}
+//     }
+//   },
+//   "interactivity": {
+//     "detect_on": "window",
+//     "events": {
+//       "onhover": {"enable": false, "mode": "bubble"},
+//       "onclick": {"enable": false, "mode": "push"},
+//       "resize": true
+//     },
+//     "modes": {
+//       "grab": {"distance": 400, "line_linked": {"opacity": 1}},
+//       "bubble": {"distance": 300, "size": 15, "duration": 2, "opacity": 0.2, "speed": 3},
+//       "repulse": {"distance": 200, "duration": 0.4},
+//       "push": {"particles_nb": 4},
+//       "remove": {"particles_nb": 2}
+//     }
+//   },
+//   "retina_detect": true
+// });
 
 // ------------------------
 // ---- Scroll Trigger ----
